30ad6d9e2bc1b4202f60731442bdf9e0cd18cd16
jmailnaif <jamailotb15@gmail.com>
2024-02-14 21:43:58 +0300

    lab

---
+++ main.py
@@ -0,0 +1,21 @@
+from clase import Panda
+panda1 = panda("kai",5,"male","mountainous forest")
+panda2 = panda("mei",4,"female","bamboo forest")
+panda3 = panda("bao bao",3,"male","temperate forest")
+panda4 = panda("lin",6,"female","subtropical forest")
+
+print("panda 1:")
+panda1.eat()
+panda1.sleep()
+
+print("panda 2:")
+panda2.eat()
+panda2.sleep()
+
+print("panda 3:")
+panda3.eat()
+panda3.sleep()
+
+print("panda 4:")
+panda4.eat()
+panda4.sleep()
